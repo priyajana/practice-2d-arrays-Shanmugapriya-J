0c63ab05c645e938d0c33b300ea5cf062f630850
Shanmugapriya <priyajanarthanan@gmail.com>
2025-03-04 15:30:17 -0600

    Added file as part of Practice: 2D Arrays

---
+++ StudentArray.js
@@ -0,0 +1,24 @@
+// Challenge 1: Student Scores
+
+let allScores = [
+    [85, 100, 98],
+    [55, 73, 81],
+    [69, 84, 72]
+];
+
+//Challenge 2: Build a Seating Grid
+let theaterSeating = [
+    ['A1','A2','A3','A4'],
+    ['B1','B2','B3','B4'],
+    ['C1','C2','C3','C4'],
+];
+
+//Challenge 3: Form a Multi-Dimensional Contact List
+let contactsList = [
+    ["James", "jolly_james@hotmail.com", "3123233256"],
+    ["Molly","mailto_molly@gmail.com", "98736582921"],
+];
+
+console.log(allScores);
+console.log(theaterSeating);
+console.log(contactsList);
